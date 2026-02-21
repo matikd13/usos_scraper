ac42601d7965d76603a914b70e312fa8ec64aad8
Mateusz Jakubowski <matikd1324@gmail.com>
2026-02-21 23:35:38 +0100

    fix

--- urls.py
+++ urls.py
@@ -6,40 +6,40 @@ YEAR = "25/26"
 def plan_url(prefix: str, grupa_part: str, term: str = "Z") -> str:
     """Build plan URL from prefix (e.g. 230-TEI), group part (e.g. 1S_sem1) and term (Z or L)."""
     cdyd = YEAR.replace("/", "%2F") + "-" + term
-    return f"{BASE}&grupa_kod={prefix}_{grupa_part}&cdyd_kod={cdyd}"
+    return f"{BASE}&grupa_kod={prefix}{grupa_part}&cdyd_kod={cdyd}"
 
 
 PLANS = {
     "teleinfa": {
         "prefix": "230-TEI",
-        "I Stopień, 1 Sem": ("1S_sem1", "Z"),
-        "I Stopień, 2 Sem": ("1S_sem2", "L"),
-        "I Stopień, 3 Sem": ("1S_sem3", "Z"),
-        "I Stopień, 4 Sem": ("1S_sem4", "L"),
-        "I Stopień, 5 Sem": ("1S_sem5", "Z"),
-        "I Stopień, 6 Sem": ("1S_sem6", "L"),
-        "I Stopień, 7 Sem": ("1S_sem7", "Z"),
-        "II stopień, 1 Sem": ("2S_sem1", "L"),
-        "II stopień, 2 Sem": ("2S_sem2", "Z"),
-        "II stopień, 3 Sem": ("2S_sem3", "L"),
+        "I Stopień, 1 Sem": ("_1S_sem1", "Z"),
+        "I Stopień, 2 Sem": ("_1S_sem2", "L"),
+        "I Stopień, 3 Sem": ("_1S_sem3", "Z"),
+        "I Stopień, 4 Sem": ("_1S_sem4", "L"),
+        "I Stopień, 5 Sem": ("_1S_sem5", "Z"),
+        "I Stopień, 6 Sem": ("_1S_sem6", "L"),
+        "I Stopień, 7 Sem": ("_1S_sem7", "Z"),
+        "II stopień, 1 Sem": ("_2S_sem1", "L"),
+        "II stopień, 2 Sem": ("_2S_sem2", "Z"),
+        "II stopień, 3 Sem": ("_2S_sem3", "L"),
     },
     "cyberka": {
         "prefix": "230-CBZ",
-        "I Stopień, 1 Sem": ("1S_sem1", "Z"),
-        "I Stopień, 2 Sem": ("1S_sem2", "L"),
-        "I Stopień, 3 Sem": ("1S_sem3", "Z"),
-        "I Stopień, 4 Sem": ("1S_sem4", "L"),
-        "I Stopień, 5 Sem": ("1S_sem5", "Z"),
-        "I Stopień, 6 Sem": ("1S_sem6", "L"),
-        "I Stopień, 7 Sem": ("1S_sem7", "Z"),
-        "II stopień, 1 Sem": ("2S_sem1", "L"),
-        "II stopień, 2 Sem": ("2S_sem2", "Z"),
-        "II stopień, 3 Sem": ("2S_sem3", "L"),
+        "I Stopień, 1 Sem": ("_1S_sem1", "Z"),
+        "I Stopień, 2 Sem": ("_1S_sem2", "L"),
+        "I Stopień, 3 Sem": ("_1S_sem3", "Z"),
+        "I Stopień, 4 Sem": ("_1S_sem4", "L"),
+        "I Stopień, 5 Sem": ("_1S_sem5", "Z"),
+        "I Stopień, 6 Sem": ("_1S_sem6", "L"),
+        "I Stopień, 7 Sem": ("_1S_sem7", "Z"),
+        "II stopień, 1 Sem": ("_2S_sem1", "L"),
+        "II stopień, 2 Sem": ("_2S_sem2", "Z"),
+        "II stopień, 3 Sem": ("_2S_sem3", "L"),
     },
     "ntwk": {
         "prefix": "230-NTK",
-        "I Stopień, 4 Sem": ("1S_sem4", "L"),
-        "I Stopień, 4 Sem, Ścieżka Chemiczna": ("1S_sem4_AK", "L"),
+        "I Stopień, 4 Sem": ("-1S_sem4", "L"),
+        "I Stopień, 4 Sem, Ścieżka Chemiczna": ("-1S_sem4_AK", "L"),
     }
 }
 
